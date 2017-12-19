a8880f2b1f36a64a09f2ce61c62fdb2d4ffc34cc
张树 <1016917515@qq.com>
2017-12-19 21:31:00 +0800

    MyBatis接口式编程

--- MyBatis_01_HelloWorld/src/com/zs/mybatis/test/MyBatisTest.java
+++ MyBatis_01_HelloWorld/src/com/zs/mybatis/test/MyBatisTest.java
@@ -10,20 +10,42 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
 import com.zs.mybatis.bean.Employee;
+import com.zs.mybatis.dao.EmployeeMapper;
 
 public class MyBatisTest {
-
-	@Test
-	public void test() throws IOException {
+	
+	public SqlSessionFactory getSqlSessionFactory() throws IOException{
 		String resource = "mybatis-config.xml";
 	    InputStream inputStream = Resources.getResourceAsStream(resource);
 	    SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-	    
+	    return sqlSessionFactory;
+	}
+
+	@Test
+	public void test() throws IOException {
+	    SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
 	    SqlSession sqlSession = sqlSessionFactory.openSession();
 	    //推介namespace+id的写法
 	    Employee employee = sqlSession.selectOne("selectEmp", 1);
 	    System.out.println(employee);
 	    sqlSession.close();
+	}
+	
+	@Test
+	public void testInterface() throws IOException{
+		//1.获取sqlSessionFactory
+		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+		//2.获取sqlSession
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		try {
+			//3.获取接口
+			EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+			//4.
+			Employee employee = mapper.getEmpById(1);
+			System.out.println(employee);
+		} finally {
+			sqlSession.close();
+		}
 	}
 
 }
